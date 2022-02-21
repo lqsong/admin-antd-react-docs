427baa511ccff3f8e7a3b97ed493c6823a73176e
lqs <957698457@qq.com>
2022-02-21 18:44:22 +0800

    add admin-vue3-micro-qiankun nav

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -36,9 +36,10 @@ module.exports = [
 		items: [
             {text: 'admin-antd-vue', link: 'http://admin-antd-vue.liqingsong.cc'},
             {text: 'admin-element-vue', link: 'http://admin-element-vue.liqingsong.cc'},
-            {text: 'electron-admin-antd-vue', link: 'http://admin-antd-vue.liqingsong.cc/guide/senior/electron.html'},
+            {text: 'electron-admin-antd-vue', link: 'http://admin-antd-vue.liqingsong.cc/webpackts/guide/senior/electron.html'},
             {text: 'electron-admin-element-vue', link: 'http://admin-element-vue.liqingsong.cc/tsv2/guide/senior/electron.html'},
             {text: 'electron-admin-antd-react', link: '/guide/senior/electron'},
+            {text: 'admin-vue3-micro-qiankun', link: 'http://admin-vue3-micro-qiankun.liqingsong.cc'},
         ]
     },
     {
